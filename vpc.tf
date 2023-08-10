e4a1e02ba5ad066caabf4c3206defa36c2b67791
kiran <kirankrishz369@gmail.com>
2023-08-10 16:00:52 +0530

    igw created, attached to vpc, add route in rt.

--- vpc.tf
+++ vpc.tf
@@ -19,6 +19,10 @@ resource "aws_subnet" "public_subnet" {
 
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.terraform_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
   tags = {
     Name = "terraform_public_rt"
   }
@@ -27,4 +31,11 @@ resource "aws_route_table" "public_rt" {
 resource "aws_route_table_association" "public_rt_association" {
   subnet_id = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.terraform_vpc.id
+  tags = {
+    Name = "terraform_igw"
+  }
 }
